15d24ec361fd174069e891a54fad85b5ea3e77bc
Nicholas Kovalchuk <dworak.niko@gmail.com>
2025-09-10 14:26:45 +0300

    added translated comments and implementing the "findByEmail" method

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -8,13 +8,19 @@ public class UserService {
             new User("alice@i.ua", "1234")
     };
 
-    /**
-     * Find user by email. All users are stored in <code>private static final User[] users</code>
-     * @param email - the input parameter
-     * @return - user if his email is equal to passed email.
-     * Return <code>null</code> if there is no suitable user
-     */
+    /*
+ Знайти користувача за електронною поштою. Усі користувачі зберігаються
+ в <code>private static final User[] users</code>
+ @param email - вхідний параметр
+  @return - користувач, якщо його електронна пошта дорівнює переданій електронній пошті.
+ Повернути <code>null</code>, якщо підходящого користувача немає
+ */
     public User findByEmail(String email) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
         return null;
     }
 }
